13132970c6619e8fa0d956e8cbe3eb1095e36044
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-01-22 19:04:13 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// import routes
+
+// const userRoutes = require("./routes/users.js");
+// const postRoutes = require("./routes/posts.js");
+// const commentRoutes = require("./Datas/comments.js");
+
+//create route for users
+
+// set up the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+/////
